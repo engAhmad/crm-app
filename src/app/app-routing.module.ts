81f7480ac84125c58c767a298bc011e627aebc76
Ahmad Al tarawneh <ahmadtarawneah@yahoo.com>
2017-06-22 03:44:51 +0300

    breadcrumbs dynamic based on routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,29 @@
-import {NgModule}              from '@angular/core';
-import {RouterModule, Routes}  from '@angular/router';
-import {HomeComponent}   from './home/home.component';
-import {FeedsComponent} from './feeds/feeds.component';
-import {LeadsComponent} from './leads/leads.component';
-import {AccountsComponent} from './accounts/accounts.component';
-import {ContactsComponent} from './contacts/contacts.component';
-import {DealsComponent} from './deals/deals.component';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {FeedsComponent} from "./feeds/feeds.component";
+import {LeadsComponent} from "./leads/leads.component";
+import {AccountsComponent} from "./accounts/accounts.component";
+import {ContactsComponent} from "./contacts/contacts.component";
+import {DealsComponent} from "./deals/deals.component";
 import {LoginComponent} from "./auth/login/login.component";
-import {ForgotPasswordComponent} from './auth/forgot-password/forgot-password.component';
-import {ReportsComponent} from './reports/reports.component';
-import {ActivitiesComponent} from './activities/activities.component';
-import {AuthGuard} from './_guards/index';
+import {ForgotPasswordComponent} from "./auth/forgot-password/forgot-password.component";
+import {ReportsComponent} from "./reports/reports.component";
+import {ActivitiesComponent} from "./activities/activities.component";
+import {AuthGuard} from "./_guards/index";
 
 const appRoutes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'login', component: LoginComponent},
-    {path: 'forgot_password', component: ForgotPasswordComponent},
-    {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-    {path: 'feeds', component: FeedsComponent, canActivate: [AuthGuard]},
-    {path: 'leads', component: LeadsComponent, canActivate: [AuthGuard]},
-    {path: 'accounts', component: AccountsComponent, canActivate: [AuthGuard]},
-    {path: 'contacts', component: ContactsComponent, canActivate: [AuthGuard]},
-    {path: 'deals', component: DealsComponent, canActivate: [AuthGuard]},
-    {path: 'reports', component: ReportsComponent, canActivate: [AuthGuard]},
-    {path: 'activities', component: ActivitiesComponent, canActivate: [AuthGuard]},
+    {path: 'login', component: LoginComponent, data: {breadcrumb: 'Login'}},
+    {path: 'forgot_password', component: ForgotPasswordComponent, data: {breadcrumb: 'Forgot password'}},
+    {path: 'home', component: HomeComponent, data: {breadcrumb: 'HOME'}, canActivate: [AuthGuard]},
+    {path: 'feeds', component: FeedsComponent, data: {breadcrumb: 'FEEDS'}, canActivate: [AuthGuard]},
+    {path: 'leads', component: LeadsComponent, data: {breadcrumb: 'LEADS'}, canActivate: [AuthGuard]},
+    {path: 'accounts', component: AccountsComponent, data: {breadcrumb: 'ACCOUNTS'}, canActivate: [AuthGuard]},
+    {path: 'contacts', component: ContactsComponent, data: {breadcrumb: 'CONTACTS'}, canActivate: [AuthGuard]},
+    {path: 'deals', component: DealsComponent, data: {breadcrumb: 'DEALS'}, canActivate: [AuthGuard]},
+    {path: 'reports', component: ReportsComponent, data: {breadcrumb: 'REPORTS'}, canActivate: [AuthGuard]},
+    {path: 'activities', component: ActivitiesComponent, data: {breadcrumb: 'ACTIVITIES'}, canActivate: [AuthGuard]}
 ];
 @NgModule({
     imports: [
--- src/app/layout/breadcrumbs/breadcrumbs.component.ts
+++ src/app/layout/breadcrumbs/breadcrumbs.component.ts
@@ -1,8 +1,60 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {TranslateService} from "@ngx-translate/core";
+import {Router, ActivatedRoute, NavigationEnd} from "@angular/router";
+import "rxjs/add/operator/filter";
+
 @Component({
     selector: 'app-breadcrumbs',
-    templateUrl: './breadcrumbs.component.html',
-})
-export class BreadcrumbsComponent {
-}
+    templateUrl: 'breadcrumbs.component.html'
 
+})
+export class BreadcrumbsComponent implements OnInit {
+    breadcrumbs: Array<Object>;
+    heading: string;
+
+    constructor(private router: Router, private route: ActivatedRoute, private translate: TranslateService) {
+        this.translate.use(this.getUserLang());
+    }
+
+    ngOnInit() {
+        this.router.events
+            .filter(event => event instanceof NavigationEnd)
+            .subscribe(event => {  // note, we don't use event
+                this.breadcrumbs = [];
+                let currentRoute = this.route.root,
+                    url = '';
+                do {
+                    let childrenRoutes = currentRoute.children;
+                    currentRoute = null;
+                    childrenRoutes.forEach(route => {
+                        if (route.outlet === 'primary') {
+                            let routeSnapshot = route.snapshot;
+                            url += '/' + routeSnapshot.url.map(segment => segment.path).join('/');
+                            this.breadcrumbs.push({
+                                label: this.getPageHeading(route.snapshot.data.breadcrumb),
+                                url: url
+                            });
+                            currentRoute = route;
+                        }
+                    })
+                } while (currentRoute);
+            });
+
+    }
+
+    getPageHeading(key: string) {
+        this.translate.get(key).subscribe((res: string) => {
+            this.heading = res;
+        });
+        return this.heading;
+    }
+
+    getUserLang() {
+        if (localStorage.getItem('currentUser')) {
+            var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+            return currentUser.language;
+        } else {
+            return this.translate.getDefaultLang();
+        }
+    }
+}
